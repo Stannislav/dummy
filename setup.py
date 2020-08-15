39b8f866a9165a4da01644ff5edbf9de716b1b33
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-08-15 12:28:18 +0100

    Improved testing setup

--- setup.py
+++ setup.py
@@ -7,5 +7,9 @@ setup(
     package_data={"dummy": ["resources/img.png", "resources/message.txt"]},
     packages=find_packages("src"),
     install_requires=["Flask", "gunicorn"],
+    extras_require={
+        "tests": ["pytest", "pytest-cov"],
+        "lint": ["bandit" "black" "flake8" "isort" "pydocstyle"],
+    },
     entry_points={"console_scripts": ["dummyweb=dummy.servers.webapp:run_my_web_app"]},
 )
